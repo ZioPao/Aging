20b0e1991362aae3403a1a63030b9a2cba4c6e06
Pao <picchetti1998@gmail.com>
2023-05-26 16:44:40 +0200

    Working in game

--- media/lua/client/Aging_Main.lua
+++ media/lua/client/Aging_Main.lua
@@ -1,2 +1,47 @@
 AgingMod = {}
 AgingMod.age = 0
+
+
+Events.OnGameStart.Add(function()
+
+    -- TODO Ask player to set age if they did not
+
+
+    local player = getPlayer()
+    local ageData = player:getModData()["AgeMod"]
+    
+    if ageData == nil or ageData == 0 then
+        player:getModData()["AgeMod"] = AgingMod.age
+    end
+end)
+
+
+
+local og_ISCharacterScreenRender = ISCharacterScreen.render
+function ISCharacterScreen:render()
+    og_ISCharacterScreenRender(self)
+
+    local z = 60
+
+    self:drawTextRight("Age", self.xOffset, z, 1,1,1,1, UIFont.Small)
+    local ageStr = tostring(getPlayer():getModData()["AgeMod"])
+    self:drawText(ageStr, self.xOffset + 10, z, 1,1,1,0.5, UIFont.Small)
+
+
+    -- self:drawTextRight(getText("IGUI_char_Weight"), self.xOffset, z, 1,1,1,1, UIFont.Small);
+    -- local weightStr = tostring(round(self.char:getNutrition():getWeight(), 0))
+    -- self:drawText(weightStr, self.xOffset + 10, z, 1,1,1,0.5, UIFont.Small);
+
+
+
+end
+
+
+
+
+-- local og_ISPlayerStatsUIRender = ISPlayerStatsUI.render
+-- function ISPlayerStatsUI:render()
+--     og_ISPlayerStatsUIRender(self)
+
+
+-- end
--- media/lua/client/OptionScreens/Aging_CharacterCreation.lua
+++ media/lua/client/OptionScreens/Aging_CharacterCreation.lua
@@ -1,19 +1,21 @@
+require "OptionScreens/CharacterCreationMain"
+require "OptionScreens/CharacterCreationHeader"
 
 local function SetHairColor(age)
-
+    print("Setting hair to age " .. tostring(age))
     local hairColors = MainScreen.instance.desc:getCommonHairColor()
-	local hairColors1 = {}
-	local info = ColorInfo.new()
+    local hairColors1 = {}
+    local info = ColorInfo.new()
 
 
     local desaturation = age/80
-	for i=1,hairColors:size() do
-		local color = hairColors:get(i-1)
-		-- we create a new info color to desaturate it (like in the game)
-		info:set(color:getRedFloat(), color:getGreenFloat(), color:getBlueFloat(), 1)
-		--		info:desaturate(0.5)
+    for i=1,hairColors:size() do
+        local color = hairColors:get(i-1)
+        -- we create a new info color to desaturate it (like in the game)
+        info:set(color:getRedFloat(), color:getGreenFloat(), color:getBlueFloat(), 1)
+        --		info:desaturate(0.5)
 
-        print(i)
+        --print(i)
 
 
         local r = info:getR() --+ greyScaler
@@ -39,14 +41,29 @@ local function SetHairColor(age)
         if b > 1 then b = 1 end
 
 
-        print("Current color: R=" .. r .. ", G=" .. g .. ", B=" .. b)
-		table.insert(hairColors1, { r=r, g=g, b=b})
-	end
+        --print("Current color: R=" .. r .. ", G=" .. g .. ", B=" .. b)
+        table.insert(hairColors1, { r=r, g=g, b=b})
+    end
     CharacterCreationMain.instance.colorPickerHair:setColors(hairColors1, math.min(#hairColors1, 10), math.ceil(#hairColors1 / 10))
+    -- TODO Force apply hair!
+    CharacterCreationMain.instance.colorPickerHair.index = 1
     CharacterCreationMain.instance.colorPickerHair:picked(true)
 
 end
 
+
+
+local og_CharacterCreationMainCreateHairTypeBtn = CharacterCreationMain.createHairTypeBtn
+function CharacterCreationMain:createHairTypeBtn()
+    og_CharacterCreationMainCreateHairTypeBtn(self)
+    --SetHairColor(25)
+    print("Forcing index 1 for hair color")
+    CharacterCreationMain.instance.colorPickerHair.index = 1
+    CharacterCreationMain.instance:removeChild( CharacterCreationMain.instance.colorPickerHair)
+	CharacterCreationMain.instance:addChild( CharacterCreationMain.instance.colorPickerHair)
+    CharacterCreationMain.instance.colorPickerHair:picked(true)
+
+end
 
 
 function CharacterCreationHeader:onTextChangeAge()
@@ -61,31 +78,26 @@ function CharacterCreationHeader:onTextChangeAge()
         if check then
             SetHairColor(age)
         end
-
-
-
-
     else
         CharacterCreationHeader.instance.ageEntry:setValid(false)
     end
 end
 
-
-
 local og_CharacterCreationHeaderCreate = CharacterCreationHeader.create
 function CharacterCreationHeader:create()
 
     og_CharacterCreationHeaderCreate(self)
+    print("Running creation header")
     local FONT_HGT_SMALL = getTextManager():getFontHeight(UIFont.Small)
     local FONT_HGT_MEDIUM = getTextManager():getFontHeight(UIFont.Medium)
-	local entryHgt = math.max(FONT_HGT_SMALL + 2 * 2, FONT_HGT_MEDIUM)
-	local labelMaxWid = 0
-	labelMaxWid = math.max(labelMaxWid, getTextManager():MeasureStringX(UIFont.Medium, getText("UI_characreation_forename")))
-	labelMaxWid = math.max(labelMaxWid, getTextManager():MeasureStringX(UIFont.Medium, getText("UI_characreation_surname")))
-	labelMaxWid = math.max(labelMaxWid, getTextManager():MeasureStringX(UIFont.Medium, getText("UI_characreation_gender")))
+    local entryHgt = math.max(FONT_HGT_SMALL + 2 * 2, FONT_HGT_MEDIUM)
+    local labelMaxWid = 0
+    labelMaxWid = math.max(labelMaxWid, getTextManager():MeasureStringX(UIFont.Medium, getText("UI_characreation_forename")))
+    labelMaxWid = math.max(labelMaxWid, getTextManager():MeasureStringX(UIFont.Medium, getText("UI_characreation_surname")))
+    labelMaxWid = math.max(labelMaxWid, getTextManager():MeasureStringX(UIFont.Medium, getText("UI_characreation_gender")))
 
-    local entryX = self.avatarPanel:getRight() + 16 + labelMaxWid + 6
-    self.ageEntry = ISTextEntryBox:new(25, self.genderCombo:getRight() + 6, self.surnameEntry:getBottom() + 6 + (entryHgt - 18) / 2, 115, entryHgt)
+    
+    self.ageEntry = ISTextEntryBox:new(tostring(25), self.genderCombo:getRight() + 6, self.surnameEntry:getBottom() + 6 + (entryHgt - 18) / 2, 115, entryHgt)
 	self.ageEntry:initialise()
 	self.ageEntry:instantiate()
     self.ageEntry:setOnlyNumbers(true)
@@ -94,7 +106,9 @@ function CharacterCreationHeader:create()
     self.ageEntry:setValid(true)
 	self:addChild(self.ageEntry)
 
-    --SetHairColor(randomAge)
+
+
+    
 end
 
 local og_CharacterCreationMainOnOptionMouseDown = CharacterCreationMain.onOptionMouseDown
@@ -114,52 +128,3 @@ function CharacterCreationMain:onOptionMouseDown(button, x, y)
 
 
 end
-
-
-
-
-local function hairColorThing()
-    local hairColors = MainScreen.instance.desc:getCommonHairColor();
-	local hairColors1 = {}
-	local info = ColorInfo.new()
-	for i=1,hairColors:size() do
-		local color = hairColors:get(i-1)
-		-- we create a new info color to desaturate it (like in the game)
-		info:set(color:getRedFloat(), color:getGreenFloat(), color:getBlueFloat(), 1)
-		--		info:desaturate(0.5)
-		table.insert(hairColors1, { r=info:getR(), g=info:getG(), b=info:getB() })
-	end
-	local hairColorBtn = ISButton:new(self.xOffset+xColor, self.yOffset, 45, comboHgt, "", self, CharacterCreationMain.onHairColorMouseDown)
-	hairColorBtn:initialise()
-	hairColorBtn:instantiate()
-	local color = hairColors1[1]
-	hairColorBtn.backgroundColor = {r=color.r, g=color.g, b=color.b, a=1}
-	self.characterPanel:addChild(hairColorBtn)
-	self.hairColorButton = hairColorBtn
-	self.yOffset = self.yOffset + comboHgt + 4;
-
-	self.colorPickerHair = ISColorPicker:new(0, 0, nil)
-	self.colorPickerHair:initialise()
-	self.colorPickerHair.keepOnScreen = true
-	self.colorPickerHair.pickedTarget = self
-	self.colorPickerHair.resetFocusTo = self.characterPanel
-	self.colorPickerHair:setColors(hairColors1, math.min(#hairColors1, 10), math.ceil(#hairColors1 / 10))
-
-
-
-end
-
-
-
-
-
-
-Events.OnGameStart.Add(function()
-
-
-    print("Initialized world, printing current age")
-    print(AgingMod.age)
-    print("Fuck off now")
-
-
-end)
